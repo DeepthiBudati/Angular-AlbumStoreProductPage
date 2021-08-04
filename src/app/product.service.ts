01e0b9ee683138c08163f6e55ded679008eebbbb
DeepthiBudati <budativenkatadeepthi@gmail.com>
2021-08-04 11:27:55 +0530

    updated changes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,21 +1,20 @@
-import { Injectable } from '@angular/core';
-import { Http, Response  } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import { Product } from './product';
-import { Album } from './album';
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import { Product } from "./product";
+import { Album } from "./album";
 @Injectable()
 export class ProductService {
+  constructor(private _http: Http) {}
+  private _albumUrl = "../assets/album.json";
+  private _productsUrl = "../assets/products.json";
 
-  constructor(private _http: Http) { }
-private _albumUrl = "../assets/album.json";
-private _productsUrl ="../assets/products.json";
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
 
-getAlbum(id: number):Observable<Album>{
-  return this._http.get(this._albumUrl).map((response)=><Album>(response.json()));
-}
-
-getProducts():Observable<Product[]>{
-  return this._http.get(this._productsUrl).map((response)=><Product[]>(response.json()));
-}
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+  }
 }
